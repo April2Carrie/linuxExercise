5d8a055d4ac9013aa626f2761de7c58f0ed42e4a
Carrie Deng <deng89@wisc.edu>
2022-10-18 22:47:34 -0500

    Get 1st task done. -Carrie

--- school.sh
+++ school.sh
@@ -1,1 +1,1 @@
-#!/bin/bash
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d ',' -f 7 | sed 's/\n/\n/g'| { sum=0;i=0;step=1; while read n; do sum=$(($sum + $n));i=$(($i+$step)); done; echo $sum,$(($sum/$i)); }
