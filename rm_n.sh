fdb5d7c567e564dd28c20f4e5a780fc59658ddbc
Bingyan Liang <bliang34@wisc.edu>
2022-10-20 15:23:26 -0500

    Delete files larger than n bytes Bingyan

--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+if [[ $# -ne 2 ]];then
+    echo "usage: $0 <dir> <n>" 1>&2
+    exit 0
+fi
+dir=$1
+filesize=$2
+
+for file in $(find $dir -type f -size +$filesize"c")
+do
+    rm -r $file
+done
