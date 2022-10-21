63d9f82da6d80f74301f952cb26a92bede4ac4b0
Shuwei Liu <sliu777@wisc.edu>
2022-10-21 04:17:40 -0500

    #!/bin/bash
    mkdir ./five
    cd ./five
    
    for i in 1 2 3 4 5
    do
        dir="dir"$i
        mkdir ./$dir
        cd ./$dir
    
        for j in 1 2 3 4
        do
    	file="file"$j
            echo  $j > $file.txt
        done
    
        cd ..
    
    done

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+mkdir ./five
+cd ./five
+
+for i in 1 2 3 4 5
+do
+    dir="dir"$i
+    mkdir ./$dir
+    cd ./$dir
+    
+    for j in 1 2 3 4
+    do
+	file="file"$j
+        echo  $j > $file.txt
+    done
+
+    cd ..
+    
+done
+
